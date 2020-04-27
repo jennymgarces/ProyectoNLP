ffd7455f03b0e57e854bea39c3b48d244ede6c5c
jennymgarces <48691195+jennymgarces@users.noreply.github.com>
2020-04-27 04:01:17 -0500

    seeding

---
+++ seeding/index.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const { Seeder } = require('mongo-seeding');
+
+const config = {
+  database: 'mongodb://127.0.0.1:27017/mydatabase',
+  dropDatabase: true,
+};
+const seeder = new Seeder(config);
+const collections = seeder.readCollectionsFromPath(
+  path.resolve('./data'),
+  {
+    transformers: [Seeder.Transformers.replaceDocumentIdWithUnderscoreId],
+  },
+);
+
+seeder
+  .import(collections)
+  .then(() => {
+    console.log('Success');
+  })
+  .catch(err => {
+    console.log('Error', err);
+  });
